3fe7da47047e27e19f88b3d33fe7cad895e37ff9
Anuj Saraogi <47737640+Anuj66@users.noreply.github.com>
2022-05-17 23:25:20 +0530

    Started adding content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import divider from './images/pattern-divider-desktop.svg'
+import iconDice from './images/icon-dice.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={"main-body"}>
+            <div className={'main-card'}>
+                <div className={'advice-header'}>
+                    <span>ADVICE #117</span>
+                </div>
+                <div className={'advice-content'}>
+                    <p>Actual Advice</p>
+                </div>
+                <div className={'advice-footer'}>
+                    <img src={divider} alt="Divider on desktop"/>
+                </div>
+                <div className={'advice-button'}>
+                    <button>
+                        <img src={iconDice} alt=""/>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;

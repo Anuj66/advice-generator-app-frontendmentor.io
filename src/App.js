f6d16c366af3c94adb761716e2b04de72f4b9222
Anuj Saraogi <47737640+Anuj66@users.noreply.github.com>
2022-05-21 00:21:08 +0530

    Added Css styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import './App.css';
-import divider from './images/pattern-divider-desktop.svg'
-import iconDice from './images/icon-dice.svg'
+import "./App.css";
+import divider from "./images/pattern-divider-desktop.svg";
+import iconDice from "./images/icon-dice.svg";
+import { useEffect, useState } from "react";
 
 function App() {
-    return (
-        <div className={"main-body"}>
-            <div className={'main-card'}>
-                <div className={'advice-header'}>
-                    <span>ADVICE #117</span>
-                </div>
-                <div className={'advice-content'}>
-                    <p>Actual Advice</p>
-                </div>
-                <div className={'advice-footer'}>
-                    <img src={divider} alt="Divider on desktop"/>
-                </div>
-                <div className={'advice-button'}>
-                    <button>
-                        <img src={iconDice} alt=""/>
-                    </button>
-                </div>
-            </div>
+  const [advice, setAdvice] = useState("");
+  const [id, setId] = useState(0);
+
+  async function getAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setAdvice(data.slip.advice);
+    setId(data.slip.id);
+  }
+
+  useEffect(() => {
+    getAdvice().then((data) => {
+      console.log(data);
+    });
+  }, []);
+
+  return (
+    <div className={"main-body"}>
+      <div className={"main-card"}>
+        <div className={"advice-header"}>
+          <h3>ADVICE #{id}</h3>
         </div>
-    );
+        <div className={"advice-content"}>
+          <p>"{advice}"</p>
+        </div>
+        <div className={"advice-footer"}>
+          <img src={divider} alt="Divider on desktop" />
+        </div>
+        <div className={"advice-button"}>
+          <button onClick={getAdvice}>
+            <img src={iconDice} alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
